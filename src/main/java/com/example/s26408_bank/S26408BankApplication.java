6cf2b33c66b9032618125a054e3fc49662a916cd
Michal000010 <157951566+Michal000010@users.noreply.github.com>
2024-01-28 12:38:02 +0100

    Initial commit

---
+++ src/main/java/com/example/s26408_bank/S26408BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s26408_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26408BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S26408BankApplication.class, args);
+    }
+
+}
